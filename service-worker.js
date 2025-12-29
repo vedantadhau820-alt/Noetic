8b275d64b9da1da2a5e3bdf4c59bae5140643a3c
vedantadhau820-alt <vedantadhau820@gmail.com>
2025-12-30 00:46:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,47 @@
-const CACHE_NAME = "buddhikosh-v1";
+const CACHE_NAME = "noetic-cache-v1";
 
-const ASSETS = [
+const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
 
-  // CSS
-  "./css/base.css",
-  "./css/cards.css",
-  "./css/navbar.css",
-  "./css/modal.css",
-  "./css/theme.css",
+  "./seed.js",
+  "./vault.js",
+  "./cards.js",
+  "./daily.js",
+  "./streak.js",
 
-  // JS
-  "./js/cards.js",
-  "./js/modals.js",
-  "./js/vault.js",
-  "./js/seed.js",
-
-  // Data files
-  "./js/data/laws.js",
-  "./js/data/psychology.js",
-  "./js/data/philosophy.js",
-  "./js/data/mentalModels.js",
-  "./js/data/powerDark.js"
+  "./base.css",
+  "./cards.css",
+  "./modal.css",
+  "./navbar.css",
+  "./theme.css"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", event => {
+  self.clients.claim();
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
-  );
-});
-
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
   );
 });
