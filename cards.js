be93fee9c3bb09aa16be5408d5886a3fc2fdffb2
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-05-12 18:32:10 +0530

    Update cards.js

--- cards.js
+++ cards.js
@@ -53,6 +53,10 @@ function createKnowledgeCard(item) {
 
   if (!item) return document.createElement("div");
 
+  if (item.read) {
+  article.classList.add("read-card");
+  }
+
   const article = document.createElement("article");
   article.className = "knowledge-card";
 
